b1a9166f4303b0ae1904f002c6209a3a12c4c096
rusgyn <rusgynmagsanay@gmail.com>
2024-03-09 23:53:56 -0500

    Implement renderTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,29 +4,65 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const createTweetElement = (tweetData) => {
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+const renderTweets = function(tweets) {
+  tweets.forEach(function(tweet) {
+    let $html = createTweetElement(tweet);
+    $('.tweets-container').append($html);
+  })
+}
+
+const createTweetElement = function(tweet) {
   const html = `
-    <header class="tweets-container-header">
-      <img class="tweeter-user-icon" src="${tweetData.user.avatars}">
-      ${tweetData.user.name};
-      <div class="tweeter-userName">${tweetData.user.handle}</div>
-    </header>
+    <article class="tweet-container">
+      <header class="tweet-container-header">
+        <img class="tweeter-user-icon" src="${tweet.user.avatars}">
+        ${tweet.user.name};
+        <div class="tweeter-userName">${tweet.user.handle}</div>
+      </header>
 
-    <section>
-      <p class="tweet-textarea-container">${tweetData.content.text}</p>
-    </section>
+      <section>
+        <p class="tweet-textarea-container">${tweet.content.text}</p>
+      </section>
 
-    <footer class="tweets-container-footer">
-      <div class="footer-left">
-        ${tweetData.created_at}
-      </div>
-      <div class="footer-right">
-        <i class="fa-solid fa-flag"></i>
-        <i class="fa-solid fa-retweet"></i>
-        <i class="fa-solid fa-heart"></i>
-      </div>
-    </footer>
+      <footer class="tweet-container-footer">
+        <div class="footer-left">
+          ${tweet.created_at}
+        </div>
+        <div class="footer-right">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>
   `;
 
-  return $(html)
+  return $(html);
 };
+
+renderTweets(data);
