8ab6a63e01c0e1b582654fefdb808d5ff14a2e55
rusgyn <rusgynmagsanay@gmail.com>
2024-03-10 00:31:52 -0500

    Add tweet form submit event handler and event.preventdefault

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,65 +4,73 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
+$(function() {
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+  
+  const renderTweets = function(tweets) {
+    tweets.forEach(function(tweet) {
+      let $html = createTweetElement(tweet);
+      $('.tweets-container').prepend($html);
+    })
   }
-]
+  
+  const createTweetElement = function(tweet) {
+    const html = `
+      <article class="tweet-container">
+        <header class="tweet-container-header">
+          <img class="tweeter-user-icon" src="${tweet.user.avatars}">
+          ${tweet.user.name};
+          <div class="tweeter-userName">${tweet.user.handle}</div>
+        </header>
+  
+        <section>
+          <p class="tweet-textarea-container">${tweet.content.text}</p>
+        </section>
+  
+        <footer class="tweet-container-footer">
+          <div class="footer-left">
+            ${tweet.created_at}
+          </div>
+          <div class="footer-right">
+            <i class="fa-solid fa-flag"></i>
+            <i class="fa-solid fa-retweet"></i>
+            <i class="fa-solid fa-heart"></i>
+          </div>
+        </footer>
+      </article>
+    `;
+  
+    return $(html);
+  };
+  
+  renderTweets(data);
 
-const renderTweets = function(tweets) {
-  tweets.forEach(function(tweet) {
-    let $html = createTweetElement(tweet);
-    $('.tweets-container').prepend($html);
-  })
-}
-
-const createTweetElement = function(tweet) {
-  const html = `
-    <article class="tweet-container">
-      <header class="tweet-container-header">
-        <img class="tweeter-user-icon" src="${tweet.user.avatars}">
-        ${tweet.user.name};
-        <div class="tweeter-userName">${tweet.user.handle}</div>
-      </header>
-
-      <section>
-        <p class="tweet-textarea-container">${tweet.content.text}</p>
-      </section>
-
-      <footer class="tweet-container-footer">
-        <div class="footer-left">
-          ${tweet.created_at}
-        </div>
-        <div class="footer-right">
-          <i class="fa-solid fa-flag"></i>
-          <i class="fa-solid fa-retweet"></i>
-          <i class="fa-solid fa-heart"></i>
-        </div>
-      </footer>
-    </article>
-  `;
-
-  return $(html);
-};
-
-renderTweets(data);
+  // Click (tweet) form submit handler.
+  $("#tweet-form").on( "submit", function( event ) {
+    alert( "Handler for `submit` called." );
+    event.preventDefault();
+  });
+});
