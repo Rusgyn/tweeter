4d068e41192b9de21b2537e1f1aa02c8a07475d5
rusgyn <rusgynmagsanay@gmail.com>
2024-07-08 21:37:14 -0400

    New script that is reponsible with our new tweet characters counter.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(function() { //runs a callback when the DOM is ready to be manipulated with jQuery.
+  const maxCount = $('.counter').html(); // => 140
+
+  $('#tweet-textarea').on('input', function() {
+    let counter = $(this).closest('form').find('.counter');// Use traverse to target the counter from 'this' (the textarea)
+    let tweet = this.value;// The character/s you press
+    let count = maxCount - tweet.length;
+
+    if (count < 0) {
+      counter.addClass('invalid');//New CSS .invalid to modify the counter color
+    } else {
+      counter.removeClass('invalid');//use the original counter color (black)
+    }
+
+    counter.html(count); //Updates the counter.
+  });
+  
+});
