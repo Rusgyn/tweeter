08799391d753944cb126e2f4cf2b5dc990635a3c
rusgyn <rusgynmagsanay@gmail.com>
2024-07-16 14:02:16 -0400

    Define createTweetElement function as require.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,13 @@
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+ * Reminder: Use (and do all your DOM work in) jQuery's document ready function */
+
+/*
+ *Within the client.js file, we're going to define a function createTweetElement that takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet. 
  */
 
+const createTweetElement = function(tweet) {
+
+  return;
+}
